9dfd2849ddfab42545c065af82ca6ab77c307780
Nguyen Ngoc Thuy Duong <77065902+56duong@users.noreply.github.com>
2026-05-13 09:15:36 +0700

    Add usage example to getDeviceId JSDoc

--- dist/esm/definitions.d.ts
+++ dist/esm/definitions.d.ts
@@ -2,6 +2,14 @@ export interface DeviceIdPlugin {
     /**
      * Get device ID and information
      * @returns Promise with device information
+     * @example
+     * import { DeviceId } from 'capacitor-device-id';
+     *
+     * DeviceId.getDeviceId().then((result) => {
+     *   console.log(result);
+     * }).catch((error) => {
+     *   console.error('Error getting device ID:', error);
+     * });
      */
     getDeviceId(): Promise<DeviceIdResult>;
 }
